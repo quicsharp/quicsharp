21ab2f415c22d6984d7e5eba3a01a9c42a307846
Paul Coignet <paulcoignet1996@gmail.com>
2019-09-24 14:31:37 +0200

    Quic skeleton

--- QuicClient.cs
+++ QuicClient.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace quicsharp
 {
     public class QuicClient
     {
+        private UdpClient client_;
+        public QuicClient()
+        {
+            client_ = new UdpClient();
+        }
+
+        // Connect to a remote server.
+        public void Connect(string ip, int port)
+        {
+            client_.Send(Encoding.ASCII.GetBytes("Hello"), 5, ip, port);
+        }
+
+        public void Close()
+        {
+            client_.Close();
+        }
     }
 }
--- QuicListener.cs
+++ QuicListener.cs
@@ -1,10 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace quicsharp
 {
     class QuicListener
     {
+        private UdpClient server_;
+        private bool started_;
+
+        public int Port { get; private set; }
+
+        public QuicListener(int port)
+        {
+            started_ = false;
+            Port = port;
+        }
+
+        // Starts the listener
+        public void Start()
+        {
+            server_ = new UdpClient(Port);
+            started_ = true;
+        }
+
+        // Close the listener
+        public void Close()
+        {
+            server_.Close();
+        }
+
+        public void Receive()
+        {
+            while (true)
+            {
+                IPEndPoint client = null;
+
+                // Listening
+                byte[] data = server_.Receive(ref client);
+                Console.WriteLine("Data received {0}:{1}.", client.Address, client.Port);
+
+                // Printing message
+                string message = Encoding.Default.GetString(data);
+                Console.WriteLine("MESSAGE : {0}\n", message);
+            }
+        }
     }
 }
