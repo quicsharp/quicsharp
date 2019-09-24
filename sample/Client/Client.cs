733ca827862c12db3ea9a8b21f67c2187872f434
MZL <marc.zundel@student.ecp.fr>
2019-09-24 15:52:04 +0200

    feat(sample): initial commit for Client

---
+++ sample/Client/Client.cs
@@ -0,0 +1,17 @@
+﻿using quicsharp;
+using System;
+using System.Net.NetworkInformation;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main()
+        {
+            QuicClient client = new QuicClient();
+
+            client.Connect("127.0.0.1", 456);
+
+        }
+    }
+}
