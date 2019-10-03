718d73aa9c8a3424a674e7c6cd21726c71d7b5fc
Paul Coignet <paulcoignet1996@gmail.com>
2019-10-03 15:04:20 +0200

    Add Variable Length integer

--- quiclib/Frames/AckFrame.cs
+++ quiclib/Frames/AckFrame.cs
@@ -9,59 +9,60 @@ namespace quicsharp.Frames
     {
         public override byte Type => 0x02;
 
-        public UInt32 LargestAcknowledged;
-        public UInt32 Delay;
-        public UInt32 AckRangeCount;
-        public UInt32 FirstAckRange;
+        public VariableLengthInteger LargestAcknowledged;
+        public VariableLengthInteger Delay;
+        public VariableLengthInteger AckRangeCount;
+        public VariableLengthInteger FirstAckRange;
 
         // Not doing AckRanges for now
 
         // ECN Counts 19.3.2
-        public UInt32 ECT0;
-        public UInt32 ECT1;
-        public UInt32 ECN_CE;
+        public VariableLengthInteger ECT0;
+        public VariableLengthInteger ECT1;
+        public VariableLengthInteger ECN_CE;
 
-        private int frameLengthBits => 8 + 7 * 32; 
+        private int frameLengthBitsMini => 8 + 7 * 8; 
 
         public override int Decode(byte[] content, int begin)
         {
-            if (content.Length < frameLengthBits + begin)
+            if (content.Length < frameLengthBitsMini + begin)
                 throw new ArgumentException("ACK Frame has a wrong size");
             if (content[begin] != Type)
                 throw new ArgumentException("Wrong frame type created");
 
-            int beginBits = begin * 8;
+            int beginBits = (begin + 1) * 8;
+            int read = 0;
 
-            LargestAcknowledged = Packet.ReadUInt32(beginBits + 8, content);
-            Delay = Packet.ReadUInt32(beginBits + 40, content);
-            AckRangeCount = Packet.ReadUInt32(beginBits + 72, content);
-            FirstAckRange = Packet.ReadUInt32(beginBits + 104, content);
+            read += LargestAcknowledged.Decode(beginBits+ read, content);
+            read += Delay.Decode(beginBits + read, content);
+            read += AckRangeCount.Decode(beginBits + read, content);
+            read += FirstAckRange.Decode(beginBits + read, content);
 
             // No AckRanges for now
 
-            ECT0 = Packet.ReadUInt32(beginBits + 136, content);
-            ECT1 = Packet.ReadUInt32(beginBits + 168, content);
-            ECN_CE = Packet.ReadUInt32(beginBits + 200, content);
+            read += ECT0.Decode(beginBits + read, content);
+            read += ECT1.Decode(beginBits + read, content);
+            read += ECN_CE.Decode(beginBits + read, content);
 
-            return 29;
+            return (read / 8);
         }
 
         public override byte[] Encode()
         {
-            byte[] content = new byte[frameLengthBits];
+            List<byte> content = new List<byte>();
 
-            Packet.WriteUInt32(8, content, LargestAcknowledged);
-            Packet.WriteUInt32(40, content, Delay);
-            Packet.WriteUInt32(72, content, AckRangeCount);
-            Packet.WriteUInt32(108, content, FirstAckRange);
+            content.AddRange(LargestAcknowledged.Encode());
+            content.AddRange(Delay.Encode());
+            content.AddRange(AckRangeCount.Encode());
+            content.AddRange(FirstAckRange.Encode());
 
             // No AckRanges for now
 
-            Packet.WriteUInt32(136, content, ECT0);
-            Packet.WriteUInt32(168, content, ECT1);
-            Packet.WriteUInt32(200, content, ECN_CE);
+            content.AddRange(ECT0.Encode());
+            content.AddRange(ECT1.Encode());
+            content.AddRange(ECN_CE.Encode());
 
-            return content;
+            return content.ToArray();
         }
     }
 }
---
+++ quiclib/VariableLengthInteger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quicsharp
+{
+    class VariableLengthInteger
+    {
+        public int Size { get; private set; }
+
+        private UInt64 value_;
+        public UInt64 Value
+        {
+            get
+            {
+                return value_;
+            }
+
+            set
+            {
+                value_ = value;
+                if (value >= (1 << 30))
+                {
+                    Size = 8;
+                }
+                else if (value >= (1 << 14))
+                {
+                    Size = 4;
+                }
+                else if (value >= (1 << 6))
+                {
+                    Size = 2;
+                }
+                else
+                {
+                    Size = 1;
+                }
+            }
+        }
+
+        VariableLengthInteger(UInt64 val)
+        {
+            Value = val;
+        }
+
+        public byte[] Encode()
+        {
+            byte[] encoded = new byte[Size];
+
+            Packet.WriteBit(0, encoded, Size >= 4);
+            Packet.WriteBit(1, encoded, Size == 2 || Size == 8);
+
+            UInt64 v = value_;
+
+            for (int i = encoded.Length - 1; i > 1; i--)
+            {
+                Packet.WriteBit(i, encoded, (v % 2) == 1);
+                v = v >> 1;
+            }
+
+            return encoded;
+        }
+
+        public int Decode(int indexBegin, byte[] data)
+        {
+            // TODO: Check input
+            Size = 0;
+           
+            switch (Packet.ReadNBits(indexBegin, data, 2))
+            {
+                case 0:
+                    Size = 1;
+                    value_ = (UInt64)Packet.ReadNBits(indexBegin + 2, data, 6);
+                    break;
+                case 1:
+                    Size = 2;
+                    value_ = (UInt64)Packet.ReadNBits(indexBegin + 2, data, 14);
+                    break;
+                case 2:
+                    Size = 4;
+                    value_ = (UInt64)Packet.ReadNBits(indexBegin + 2, data, 30);
+                    break;
+                case 3:
+                    Size = 8;
+                    value_ = (UInt64)Packet.ReadNBits(indexBegin + 2, data, 62);
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            return Size * 8;
+        }
+    }
+}
