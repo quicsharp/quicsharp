b384c4035a85937ab7bb9f827deed86cda582328
Paul Coignet <paulcoignet1996@gmail.com>
2019-11-21 16:14:49 +0100

    Doc Frame.cs

--- quiclib/Frame.cs
+++ quiclib/Frame.cs
@@ -13,8 +13,18 @@ namespace quicsharp
         // Section 12.3 Table 3
         public abstract byte Type { get; }
 
+        /// <summary>
+        /// Abstract method to encode a specific frame to a raw byte array
+        /// </summary>
+        /// <returns>The encoded frame</returns>
         public abstract byte[] Encode();
-        // Return the number of byte read
+
+        /// <summary>
+        /// Abstract method to decode a specific from a raw byte array
+        /// </summary>
+        /// <param name="content">The raw byte array</param>
+        /// <param name="begin">The bit index of the byte array where the AckFrame is located</param>
+        /// <returns>The number of bits read</returns>
         public abstract int Decode(byte[] payload, int begin);
     }
 }
